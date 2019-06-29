8fa643de79daff32fed148f80ed0c19c0df10c9d
guangtoutou <ni.ningning@gmail.com>
2019-06-29 15:49:10 +1000

    Initial Commit
    created a hello controller

---
+++ src/main/java/com/nilab/fourstagespipeline/FourstagespipelineApplication.java
@@ -0,0 +1,13 @@
+package com.nilab.fourstagespipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourstagespipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourstagespipelineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nilab/fourstagespipeline/HelloController.java
@@ -0,0 +1,14 @@
+package com.nilab.fourstagespipeline;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public ResponseEntity<String> sayHello(){
+		return ResponseEntity.ok("Hello");
+	}
+}
